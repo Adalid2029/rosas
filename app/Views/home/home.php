3b81f7a222d0a96dcc78bf99dc015e53f6df869e
Juan Carlos Condori <jcondori92@outlook.es>
2020-11-27 22:09:20 -0400

    modificacion del home y menu

--- app/Views/home/home.php
+++ app/Views/home/home.php
@@ -4,10 +4,70 @@
             <div class="panel-heading">
                 <div class="panel-control">
                 </div>
-                <h3 class="panel-title">Principal</h3>
+                <h3 class="panel-title">Página Principal</h3>
             </div>
             <div class="panel-body">
-                Contenido de la pagina principal
+
+                <div class="row">
+                    <div class="col-lg-12">
+
+                        <div class="col-lg-3 col-md-6">
+                            <!--Tile-->
+                            <!--===================================================-->
+                            <div class="panel panel-primary panel-colorful">
+                                <div class="pad-all text-center">
+                                    <span class="text-3x text-thin">53</span>
+                                    <p>Sales</p>
+                                    <i class="demo-pli-shopping-bag icon-lg"></i>
+                                </div>
+                            </div>
+                            <!--===================================================-->
+                        </div>
+
+                        <div class="col-lg-3 col-md-6">
+                            <!--Tile-->
+                            <!--===================================================-->
+                            <div class="panel panel-warning panel-colorful">
+                                <div class="pad-all text-center">
+                                    <span class="text-3x text-thin">68</span>
+                                    <p>Messages</p>
+                                    <i class="demo-psi-mail icon-lg"></i>
+                                </div>
+                            </div>
+                            <!--===================================================-->
+                        </div>
+
+                        <div class="col-lg-3 col-md-6">
+                            <!--Tile-->
+                            <!--===================================================-->
+                            <div class="panel panel-purple panel-colorful">
+                                <div class="pad-all text-center">
+                                    <span class="text-3x text-thin">32</span>
+                                    <p>Projects</p>
+                                    <i class="demo-pli-coding"></i>
+                                </div>
+                            </div>
+                            <!--===================================================-->
+                        </div>
+
+
+                        <div class="col-lg-3 col-md-6">
+                            <!--Tile-->
+                            <!--===================================================-->
+                            <div class="panel panel-dark panel-colorful">
+                                <div class="pad-all text-center">
+                                    <span class="text-3x text-thin">12</span>
+                                    <p>Reports</p>
+                                    <i class="demo-psi-receipt-4 icon-lg"></i>
+                                </div>
+                            </div>
+                            <!--===================================================-->
+                        </div>
+
+                    </div>
+
+                </div>
+
             </div>
         </div>
     </div>
