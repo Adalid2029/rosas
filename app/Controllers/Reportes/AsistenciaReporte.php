ffc40f8c3c64cfc08d863429c013bcb880677313
Juan Carlos Condori <jcondori92@outlook.es>
2020-12-02 10:34:58 -0400

    correccion de header de reporte de asistencia

--- app/Controllers/Reportes/AsistenciaReporte.php
+++ app/Controllers/Reportes/AsistenciaReporte.php
@@ -10,37 +10,38 @@ class AsistenciaReporte extends FPDF
     public function imprimir($data = null)
     {
         $this->AddPage('L', 'legal');
-        $this->Image("img/images/logo_oficial.png", 60 ,14, 15 , 15,'PNG', '');
-        $this->SetFont('Arial', 'B', 13);
-        $this->Cell(0, 3, utf8_decode('CUADRO DE ASISTENCIA"'), 0, 1, 'C', 0);
+        $this->Image("img/images/logo_oficial.png", 50 ,14, 15 , 15,'PNG', '');
+        $this->SetFont('Arial', 'BU', 13);
+        $this->Cell(0, 3, utf8_decode('CUADRO DE ASISTENCIA'), 0, 1, 'C', 0);
         $this->Ln();
         $this->SetFont('Arial', '', 12);
-        $this->SetX(80);
+        $this->SetX(70);
         $this->SetFont('Arial', 'B', 12);
         $this->Cell(45,5,'UNIDAD EDUCATIVA: ',0,0,'R',0);
         $this->SetFont('Arial', '', 12);
         $this->Cell(35,5,'LAS ROSAS',0,0,'L',0);
         $this->SetFont('Arial', 'B', 12);
-        $this->Cell(30,5,'CURSO: ',0,0,'R',0);
+        $this->Cell(45,5,'CURSO: ',0,0,'R',0);
         $this->SetFont('Arial', '', 12);
         $this->Cell(70,5,'TERCERO "A" SECUNDARIA',0,0,'L',0);
         $this->SetFont('Arial', 'B', 12);
         $this->Cell(30,5,utf8_decode('GESTIÓN: '),0,0,'R',0);
         $this->SetFont('Arial', '', 12);
         $this->Cell(30,5,'2020',0,0,'L',0);
+        $this->Ln(7);
+        $this->SetX(75);
+        $this->SetFont('Arial', 'B', 12);
+        $this->Cell(45,5,'DE: ',0,0,'R',0);
+        $this->SetFont('Arial', '', 12);
+        $this->Cell(35,5,'24-05-2020  a 30-05-2020',0,0,'L',0);
+        $this->SetFont('Arial', 'B', 12);
+        $this->Cell(45,5,'IMPRESO: ',0,0,'R',0);
+        $this->SetFont('Arial', '', 12);
+        $this->Cell(70,5,'20-05-2020',0,0,'L',0);
+        $this->SetFont('Arial', 'B', 12);
 
         $this->Ln(10);
         $this->Output();
-    }
-
-    function imprimirCabecera($data)
-    {
-        // Datos
-        foreach($data as $row)
-        {
-            $this->Cell(40,6,$row,1);
-            $this->Ln();
-        }
     }
 
 }
