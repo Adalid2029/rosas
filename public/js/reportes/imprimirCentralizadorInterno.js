d88cf2df7c715c7056646decdb2aa718e4996a65
Stack News <silviahello1995@gmail.com>
2020-12-15 10:43:55 -0400

    dsf

--- public/js/reportes/imprimirCentralizadorInterno.js
+++ public/js/reportes/imprimirCentralizadorInterno.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 	$('.seleccion-materia').on('click', function () {
-		$.get('/notas/imprimirCentralizadorInterno').done(function (r) {
+		$.get('/notas/imprimirCentralizadorInterno', {}).done(function (r) {
 			$('#centralizador-interno-body').html('<embed type="application/pdf" src="' + r.exito + '" width="100%" height="' + $(window).height() + '" style="border: none;" />');
 			parametrosModal('#centralizador-interno', 'Editar notas del Estudiante: ', 'modal-lg', false, 'static');
 		});
